c1b58fee954b90de1d5db609866bcf6b33b9793f
Dima <pechenenkod@gmail.com>
2022-09-13 17:27:16 +0300

    Add new project

---
+++ twitter_concept_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twitter_concept_SwiftUI
+//
+//  Created by Dima Pechenenko on 13.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twitter_concept_SwiftUI/twitter_concept_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_concept_SwiftUIApp.swift
+//  twitter_concept_SwiftUI
+//
+//  Created by Dima Pechenenko on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct twitter_concept_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
